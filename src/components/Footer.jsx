a72996d7a5cba046c158f332ca7c38e90ccf558b
vighneshadelkar <vighnesh.adelkar21@gmail.com>
2024-04-11 14:03:24 +0530

    remove unused imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "../style/Footer.css";
-import { Link } from "react-router-dom";
 
 export default function Footer() {
   const toGithub = (url) => {
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "../style/Projects.css";
 import { useNavigate } from "react-router-dom";
 
